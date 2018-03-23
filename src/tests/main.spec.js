128cba7d5d5cc2bd232a3f3bcaabf4a7430e239d
Aldo Soares <soares_aldo@hotmail.com>
2018-03-22 23:16:35 -0300

    [IMP] Criação de 'smoke' tests para criação de calculadora

--- src/tests/main.spec.js
+++ src/tests/main.spec.js
@@ -1,52 +1,34 @@
 const expect = require('chai').expect;
+const calc = require('../main.js');
 
-describe('Main', () => {
-  let arr;
+describe('Calc', () => {
 
-  // roda uma vez antes do bloco
-  before( () => {
-    // inicia uma conexão no banco
-    // criar um conjunto de dados
+  // smoke tests
+  describe('Smoke Tests', () => {
+
+    it('should exist "calc" lib', () => {
+      expect(calc).to.exist;
+    });
+
+    it('should exist method "sum"', () => {
+      expect(calc.sum).to.exist;
+      expect(calc.sum).to.be.a('function');
+    });
+
+    it('should exist method "sub"', () => {
+      expect(calc.sub).to.exist;
+      expect(calc.sub).to.be.a('function');
+    });
+
+    it('should exist method "div"', () => {
+      expect(calc.div).to.exist;
+      expect(calc.div).to.be.a('function');
+    });
+
+    it('should exist method "mult"', () => {
+      expect(calc.mult).to.exist;
+      expect(calc.mult).to.be.a('function');
+    });
+
   });
-
-  // roda uma vez, depois do bloco
-  after( () => {
-    // fecha a conexão do banco
-    // apagar esse conjunto de dados
-  });
-
-  // roda todas as vezes, antes de CADA bloco
-  beforeEach( () => {
-    arr = [1, 2, 3];
-  });
-
-  // roda todas as vezes, depois de CADA bloco
-  afterEach( () => {
-
-  });
-
-  // testar tipos ou se existe (smoke test)
-  it('should be an array', () => {
-    expect(arr).to.be.a('array');
-  });
-
-  it('should return true when pop in the array', () => {
-    expect(arr.pop() === 3).to.be.true;
-  });
-
-  it('should have a size of 4 when push another value to the array', () => {
-    arr.push(4);
-    expect(arr).to.have.lengthOf(4); // 4
-  });
-
-  it('should remove the value 3 when use pop in the array', () => {
-    arr.pop();
-    expect(arr).to.not.include(3);
-  });
-
-  it('should remove the value 3 when use pop in the array', () => {
-    arr.pop();
-    expect(arr).to.have.lengthOf(2); // 2
-  });
-
 });
